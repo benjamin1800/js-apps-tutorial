5bd1cbf546a75e555e85bbf733792a03bdcb3190
Divine Ikhuoria <divuzki@gmail.com>
2024-05-31 21:54:58 +0100

    Added Remove Functionality

--- theOdinProject/bookLibrary/script.js
+++ theOdinProject/bookLibrary/script.js
@@ -11,8 +11,8 @@ form.addEventListener("submit", (event) => {
   displayBooks();
 });
 
-function Books(title, author, pages) {
-  const bookObj = { title, author, pages };
+function Books(id, title, author, pages) {
+  const bookObj = { id, title, author, pages };
   return bookObj;
 }
 
@@ -20,23 +20,42 @@ function addBookToLibrary() {
   const title = document.getElementById("name").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
+  const id = myLibrary.length + 1;
 
-  const addedBook = Books(title, author, pages);
+  const addedBook = Books(id, title, author, pages);
   myLibrary.push(addedBook);
 }
 
 function displayBooks() {
+  library.innerHTML = "";
   for (let i = 0; i < myLibrary.length; i++) {
     const book = myLibrary[i];
-    bookTemplate(book.title, book.author, book.pages);
+    bookTemplate(book.id, book.title, book.author, book.pages);
   }
 }
 
-function bookTemplate(title, author, pages) {
+function bookTemplate(id, title, author, pages) {
   const template = `
   <div> Book Title: ${title}</div>
   <div> Book Author: ${author}</div>
   <div> Book Pages: ${pages}</div>
+  
+  <div>
+  <button class="remove-btn" book-id="${id}">Remove</button>
+  </div>
  `;
   library.innerHTML = template;
+}
+
+library.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-btn")) {
+    const bookId = event.target.getAttribute("book-id");
+    removeBook(bookId);
+  }
+});
+
+function removeBook(id) {
+  const bookIndex = myLibrary.findIndex((book) => book.id === parseInt(id));
+  myLibrary.splice(bookIndex, 1);
+  displayBooks();
 }
