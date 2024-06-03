f702bb8b6a5aa0679661bd2458da2744f89cafe9
emmanuelbenjamin <emmanuelbenjamin1800@gmail.com>
2024-06-03 20:08:46 +0100

    new book library

--- theOdinProject/bookLibrary/script.js
+++ theOdinProject/bookLibrary/script.js
@@ -1,42 +1,72 @@
-const form = document.getElementById("add-form");
-const library = document.querySelector("#book-container");
+let myLibrary = [];
 
-let read = false;
-const myLibrary = [];
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  addBookToLibrary();
-  form.reset();
-  displayBooks();
-});
-
-function Books(title, author, pages) {
-  const bookObj = { title, author, pages };
-  return bookObj;
+function Book(title, author, pages, read) {
+   this.title = title;
+   this.author = author;
+   this.pages = pages;
+   this.read = read;
 }
 
-function addBookToLibrary() {
-  const title = document.getElementById("name").value;
-  const author = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-
-  const addedBook = Books(title, author, pages);
-  myLibrary.push(addedBook);
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
 }
 
-function displayBooks() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    const book = myLibrary[i];
-    bookTemplate(book.title, book.author, book.pages);
+
+function toggleRead(index){
+  myLibrary[index].toggleRead();
+  render()
+
+}
+
+
+function render() {
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++){
+    let book = myLibrary[i];
+    let bookEl = document.createElement("div");
+    bookEl.setAttribute("class", "book-card");
+    bookEl.innerHTML = `
+    <div class="card-header">
+    <h3 class="title">${book.title}</h3>
+    <h5 class="author">by ${book.author}</h5>
+    </div>
+    <div class="card-body">
+    <p>${book.pages} pages</p>
+    <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+    <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+    <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
+  
+    </div>
+    `;
+    libraryEl.appendChild(bookEl);
   }
 }
 
-function bookTemplate(title, author, pages) {
-  const template = `
-  <div> Book Title: ${title}</div>
-  <div> Book Author: ${author}</div>
-  <div> Book Pages: ${pages}</div>
- `;
-  library.innerHTML = template;
+function removeBook(index) {
+  myLibrary.splice(index, 1)
+  render()
 }
+
+function addBookToLibrary() {
+  // do stuff here
+  let title = document.querySelector("#title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  render();
+}
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function(){
+   let newBookForm = document.querySelector("#new-book-form");
+   newBookForm.style.display = "block";
+
+})
+ 
+document.querySelector("#new-book-form").addEventListener("submit", function(event){
+  event.preventDefault();
+  addBookToLibrary();
+})
