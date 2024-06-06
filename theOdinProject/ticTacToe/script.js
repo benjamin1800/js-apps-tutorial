f2c3b5321fe59c2ab88531998e2323b6f0797469
emmanuelbenjamin <emmanuelbenjamin1800@gmail.com>
2024-06-06 12:30:41 +0100

    Tic Tac Toe

--- theOdinProject/ticTacToe/script.js
+++ theOdinProject/ticTacToe/script.js
@@ -1,47 +1,48 @@
 const displayController = (() => {
-    const renderMessage = (message) => {
-      document.querySelector("#message").innerHTML = message;
-    }
-    return {
-      renderMessage,
-    }
+  const renderMessage = (message) => {
+    document.querySelector("#message").innerHTML = message;
+  };
+  return {
+    renderMessage,
+  };
 })();
 
 const Gameboard = (() => {
-  let gameboard = ["", "", "", "", "", "", "", "", ""]
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
 
   const render = () => {
     let boardHTML = "";
     gameboard.forEach((square, index) => {
-      boardHTML += `<div class="square" id="square-${index}">${square}</div>`
+      boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
     });
     document.querySelector("#gameboard").innerHTML = boardHTML;
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
-        square.addEventListener("click", Game.handleClick);
-    })
-    
-  }
+      square.addEventListener("click", Game.handleClick);
+    });
+  };
 
-const update = (index, value) => {
+  const update = (index, value) => {
     gameboard[index] = value;
     render();
-};
+  };
 
-const getGameboard = () => gameboard;
+  const getGameboard = () => {
+    return gameboard
+  }
 
   return {
     render,
     update,
-    getGameboard
-  }
+    getGameboard,
+  };
 })();
 
-const createPlayer = (name, robert) => {
+const createPlayer = (name, mark) => {
   return {
     name,
-    robert
-  }
+    mark,
+  };
 };
 
 const Game = (() => {
@@ -52,85 +53,90 @@ const Game = (() => {
   const start = () => {
     players = [
       createPlayer(document.querySelector("#player1").value, "X"),
-      createPlayer(document.querySelector("#player2").value, "O")
+      createPlayer(document.querySelector("#player2").value, "O"),
     ];
     currentPlayerIndex = 0;
     gameOver = false;
     Gameboard.render();
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
-        square.addEventListener("click", handleClick);
-    })
-  }
+      square.addEventListener("click", handleClick);
+    });
+  };
 
   const handleClick = (event) => {
-     if (gameOver) {
-        return;
-     }
-    let index = parseInt(event.target.id.split("-")[1]);
-    if (Gameboard.getGameboard()[index] !== "")
+    if (gameOver) {
       return;
+    }
+    
+    let index = parseInt(event.target.id.split("-")[1]);
+   
+    if (Gameboard.getGameboard()[index] !== "") return;
 
 
     Gameboard.update(index, players[currentPlayerIndex].mark);
 
-    if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)){
-        gameOver = true;
-        displayController.renderMessage(`${players[currentPlayerIndex].name}wins`)
-    } else if (checkForTie(Gameboard.getGameboard())){
-        gameOver = true;
-        displayController.renderMessage("It's a tie");
+    if (
+      checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)
+    ) {
+      gameOver = true;
+      displayController.renderMessage(
+        `${players[currentPlayerIndex].name}wins`
+      );
+    } else if (checkForTie(Gameboard.getGameboard())) {
+      gameOver = true;
+      displayController.renderMessage("It's a tie");
     }
 
     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
-  }
+  };
 
   const restart = () => {
     for (let i = 0; 1 < 9; i++) {
-        Gameboard.update(i, "");
+      Gameboard.update(i, "");
     }
     Gameboard.render();
     gameOver = false;
     document.querySelector("#message").innerHTML = "";
-  }
+  };
 
   return {
     start,
     restart,
-    handleClick
-  }
+    handleClick,
+  };
 })();
 
 function checkForWin(board) {
-    const winningCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-    for ( let i = 0; i < winningCombinations.length; i++) {
-        const [a,b, c] = winningCombinations[i];
-        if (board[a] && board[a]  === board[b] && board[a] === board[c]){
-          return true;
-      }
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function checkForTie(board) {
-    return board.every(cell => cell !== "")
+  return board.every((cell) => cell !== "");
 }
 
 const restartButton = document.querySelector("#restart-button");
 restartButton.addEventListener("click", () => {
-    Game.restart();
-})
+  Game.restart();
+});
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
   Game.start();
-})
+});
